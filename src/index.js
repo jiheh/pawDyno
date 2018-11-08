56dbfe046102543f5ee6da3ccf176a688ce7588f
Jiheh Ritterling <jritterling@gmail.com>
2018-11-08 16:17:59 -0500

    left arm working

--- src/index.js
+++ src/index.js
@@ -2,57 +2,65 @@
 
 require('./style.css');
 
-var app = new PIXI.Application(window.innerWidth, window.innerHeight);
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: 	0x4fbdf2
+});
 document.body.appendChild(app.view);
 
+// Create a container for every animal
+let dogBody = PIXI.Sprite.fromImage('images/dogBody.svg');
+let dogLeftArm = PIXI.Sprite.fromImage('images/leg.svg');
+let dogRightArm = PIXI.Sprite.fromImage('images/leg.svg');
+let dogLeftLeg = PIXI.Sprite.fromImage('images/leg.svg');
+let dogRightLeg = PIXI.Sprite.fromImage('images/leg.svg');
 
-// PASTED EXAMPLE
-var ropeLength = 45;
-var points = [];
+dogBody.width = 60.71;
+dogBody.height = 164.6;
+dogBody.x = app.screen.width / 2 - dogBody.width;
+dogBody.y = app.screen.height - dogBody.height;
 
-for (var i = 0; i < 25; i++) {
-  points.push(new PIXI.Point(i * ropeLength, 0));
-}
+dogLeftArm.anchor.set(1);
+dogLeftArm.rotation += -.3;
+dogLeftArm.height = 89.8;
+dogLeftArm.width = 13.1;
+dogLeftArm.x = dogBody.x + dogLeftArm.width / 4;
+dogLeftArm.y = dogBody.y + 100;
 
-var strip = new PIXI.mesh.Rope(PIXI.Texture.EMPTY, points);
-strip.x = -40;
-strip.y = 300;
+dogRightArm.rotation = .3;
+dogRightArm.x = dogBody.x + dogBody.width - 8;
+dogRightArm.y = dogBody.y + 30;
 
-app.stage.addChild(strip);
+// dogLeftLeg.rotation = .3;
+// dogLeftLeg.x = 150;
+// dogLeftLeg.y = 150;
+//
+// dogRightLeg.rotation = .3;
+// dogRightLeg.x = 200;
+// dogRightLeg.y = 200;
 
-var g = new PIXI.Graphics();
-g.x = strip.x;
-g.y = strip.y;
-app.stage.addChild(g);
+app.stage.addChild(dogBody);
+app.stage.addChild(dogLeftArm);
+app.stage.addChild(dogRightArm);
+// app.stage.addChild(dogLeftLeg);
+// app.stage.addChild(dogRightLeg);
 
-// start animating
-var count = 0;
+let xScale = 1;
+let yScale = 1;
 
-app.ticker.add(function() {
-  count += 0.1;
+let targetX = 100;
+let targetY = 100;
 
-  // make the snake
-  for (var i = 0; i < points.length; i++) {
-    points[i].y = Math.sin((i * 0.5) + count) * 30;
-    points[i].x = i * ropeLength + Math.cos((i * 0.3) + count) * 20;
+xScale = (dogLeftArm.x - targetX) / dogLeftArm.width;
+yScale = (dogLeftArm.y - targetY) / dogLeftArm.height;
+
+mainLoop();
+
+function mainLoop() {
+  if (dogLeftArm.scale.y < yScale) {
+    dogLeftArm.scale.y += .1;
   }
 
-  renderPoints();
-});
-
-function renderPoints () {
-  g.clear();
-
-  g.lineStyle(2,0xffc2c2);
-  g.moveTo(points[0].x,points[0].y);
-
-  for (var i = 1; i < points.length; i++) {
-    g.lineTo(points[i].x,points[i].y);
-  }
-
-  for (var i = 1; i < points.length; i++) {
-    g.beginFill(0xff0022);
-    g.drawCircle(points[i].x,points[i].y,10);
-    g.endFill();
-  }
+  requestAnimationFrame(mainLoop);
 }

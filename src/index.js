2acbef9779ee7384bcc1c28134c8b9430c05f9ed
Jacklyn Liu <jacklyn.liu4057@gmail.com>
2018-11-08 16:12:42 -0500

    Arms and ratios

--- src/index.js
+++ src/index.js
@@ -2,57 +2,43 @@
 
 require('./style.css');
 
-var app = new PIXI.Application(window.innerWidth, window.innerHeight);
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor:  0x4fbdf2
+});
 document.body.appendChild(app.view);
 
+var bunny = PIXI.Sprite.fromImage('images/BunnyBody.svg')
+bunny.x = 500;
+bunny.y = 100;
+bunny.height = 200
+bunny.width = 100
 
-// PASTED EXAMPLE
-var ropeLength = 45;
-var points = [];
+var bunny_larm = PIXI.Sprite.fromImage('images/LArm.svg')
+bunny_larm.x = bunny.x + bunny.width/4;
+bunny_larm.y = bunny.y + bunny.height/1.5;
+bunny_larm.anchor.x=1
+bunny_larm.anchor.y = 1
 
-for (var i = 0; i < 25; i++) {
-  points.push(new PIXI.Point(i * ropeLength, 0));
-}
+var bunny_rarm = PIXI.Sprite.fromImage('images/RArm.svg')
+bunny_rarm.x = bunny.x + bunny.width/1.5;
+bunny_rarm.y = bunny.y + bunny.height/1.5;
+bunny_rarm.anchor.x=0
+bunny_rarm.anchor.y = 1
 
-var strip = new PIXI.mesh.Rope(PIXI.Texture.EMPTY, points);
-strip.x = -40;
-strip.y = 300;
+var bunny_leg = PIXI.Sprite.fromImage('images/BunnyLegs.svg')
+bunny_leg.x = bunny.x;
+bunny_leg.y = bunny.y + bunny.height;
 
-app.stage.addChild(strip);
 
-var g = new PIXI.Graphics();
-g.x = strip.x;
-g.y = strip.y;
-app.stage.addChild(g);
+app.stage.addChild(bunny);
+app.stage.addChild(bunny_larm);
+app.stage.addChild(bunny_rarm)
+app.stage.addChild(bunny_leg);
 
-// start animating
-var count = 0;
+/*app.ticker.add(function(delta) {
+  bunny_larm.scale.y += delta*.01
+  bunny_larm.scale.x += delta *.01
 
-app.ticker.add(function() {
-  count += 0.1;
-
-  // make the snake
-  for (var i = 0; i < points.length; i++) {
-    points[i].y = Math.sin((i * 0.5) + count) * 30;
-    points[i].x = i * ropeLength + Math.cos((i * 0.3) + count) * 20;
-  }
-
-  renderPoints();
-});
-
-function renderPoints () {
-  g.clear();
-
-  g.lineStyle(2,0xffc2c2);
-  g.moveTo(points[0].x,points[0].y);
-
-  for (var i = 1; i < points.length; i++) {
-    g.lineTo(points[i].x,points[i].y);
-  }
-
-  for (var i = 1; i < points.length; i++) {
-    g.beginFill(0xff0022);
-    g.drawCircle(points[i].x,points[i].y,10);
-    g.endFill();
-  }
-}
+})*/

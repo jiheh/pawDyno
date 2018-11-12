ce1f0ae082c0c71d9a669780f4584f90c233f4b5
Katie Silverio <astrosilverio@gmail.com>
2018-11-12 14:34:52 -0500

    Recalculate y position based on initial viewport window

--- src/index.js
+++ src/index.js
@@ -5,23 +5,25 @@ require('./style.css');
 import {initializeCharacters, renderCharacters} from './character';
 import {initializeWall} from './wall';
 
-export const VIEWPORT_WIDTH = window.innerWidth;
+export const WIDTH = window.innerWidth;
 export const VIEWPORT_HEIGHT = window.innerHeight;
 export const NUM_PLAYERS = 4;
+export const GAMEBOARD_HEIGHT = VIEWPORT_HEIGHT * 2;
+const START_YPOS = GAMEBOARD_HEIGHT - VIEWPORT_HEIGHT;
 
 var app = new PIXI.Application({
-  width: VIEWPORT_WIDTH,
+  width: WIDTH,
   height: VIEWPORT_HEIGHT,
   backgroundColor:  0x555555
 });
 
-function renderWall () {
+function renderWall (min_ypos) {
   let wall = initializeWall();
 
 	for(let hold of wall){
 		let text = new PIXI.Text(hold.label, {fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
 		text.x = hold.x
-		text.y = hold.y
+		text.y = hold.y - min_ypos
 		app.stage.addChild(text)
   }
 }
@@ -33,12 +35,16 @@ export function renderGameObject(gameObject) {
 // Game Setup
 function startGame() {
   document.body.appendChild(app.view);
+<<<<<<< HEAD
 
   renderWall();
 
   let characters = initializeCharacters(NUM_PLAYERS);
   renderCharacters(characters);
 
+=======
+  renderWall(START_YPOS);
+>>>>>>> Recalculate y position based on initial viewport window
   mainLoop();
 }
 

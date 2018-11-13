63497703ddb9ffe107d5f60e1f2e17d18688a2c3
Jiheh Ritterling <jritterling@gmail.com>
2018-11-13 01:19:20 -0500

    paws replacing legs

--- src/character.js
+++ src/character.js
@@ -5,8 +5,10 @@ import {
   renderGameObject
 } from './index';
 
-const characterHeight = 160;
-const characterWidth = 60;
+const characterHeight = 110;
+const characterWidth = 52;
+const pawHeight = 20;
+const pawWidth = 20;
 
 class Character {
   constructor(body, leftArm, rightArm, leftLeg, rightLeg) {
@@ -36,13 +38,10 @@ class Character {
 }
 
 class CharacterPart {
-  constructor(imageSrc, height, width, rotation, anchorX, anchorY, x, y) {
+  constructor(imageSrc, height, width, x, y) {
     this.imageSrc = imageSrc;
     this.height = height;
     this.width = width;
-    this.rotation = rotation;
-    this.anchorX = anchorX;
-    this.anchorY = anchorY;
     this.x = x;
     this.y = y;
   }
@@ -72,15 +71,16 @@ function moveRightLeg(character, targetX, targetY) {
 export function initializeCharacters(numPlayers) {
   let characters = new Array(numPlayers).fill(null);
 
+  // TODO: Better calculations for (x, y) of parts
   return characters.map((_, idx) => {
     let bodyX = VIEWPORT_WIDTH / NUM_PLAYERS * (idx + 1) * .75 + (characterWidth / 2);
-    let bodyY = VIEWPORT_HEIGHT - characterHeight / 2;
+    let bodyY = VIEWPORT_HEIGHT - characterHeight;
 
-    let body = new CharacterPart(`images/character${idx}/body.svg`, characterHeight, characterWidth, 0, .5, .5, bodyX, bodyY);
-    let lArm = new CharacterPart(`images/character${idx}/leg.svg`, 90, 13, -.75, 0, 1, bodyX, bodyY);
-    let rArm = new CharacterPart(`images/character${idx}/leg.svg`, 90, 13, .75, 1, 1, bodyX, bodyY);
-    let lLeg = new CharacterPart(`images/character${idx}/leg.svg`, 90, 13, -2.45, 1, 1, bodyX, bodyY);
-    let rLeg = new CharacterPart(`images/character${idx}/leg.svg`, 90, 13, 2.45, 0, 1, bodyX, bodyY);
+    let body = new CharacterPart(`images/character${idx}/body.svg`, characterHeight, characterWidth, bodyX, bodyY);
+    let lArm = new CharacterPart(`images/character${idx}/paw.svg`, pawHeight, pawWidth, bodyX - characterWidth / 4.25, bodyY + characterHeight / 4);
+    let rArm = new CharacterPart(`images/character${idx}/paw.svg`, pawHeight, pawWidth, bodyX + characterWidth * .9, bodyY + characterHeight / 4);
+    let lLeg = new CharacterPart(`images/character${idx}/paw.svg`, pawHeight, pawWidth, bodyX - characterWidth / 4.25, bodyY + characterHeight / 1.5);
+    let rLeg = new CharacterPart(`images/character${idx}/paw.svg`, pawHeight, pawWidth, bodyX + characterWidth * .9, bodyY + characterHeight / 1.5);
 
     return new Character(body, lArm, rArm, lLeg, rLeg);
   });
@@ -99,7 +99,7 @@ function characterToSprites(character) {
 
   return bodyParts.map(bp => {
     return bodyPartToSprite(character[bp]);
-  });
+  }).reverse();
 }
 
 function bodyPartToSprite(part) {
@@ -109,8 +109,6 @@ function bodyPartToSprite(part) {
   sprite.width = part.width;
   sprite.x = part.x;
   sprite.y = part.y;
-  sprite.rotation += part.rotation;
-  sprite.anchor.set(part.anchorX, part.anchorY);
 
   return sprite;
 }
